3b2b63e12724fc59661d99c755ab628a67f6eb62
GhostsonAcid <GhostsonAcid@users.noreply.github.com>
2026-02-07 16:34:08 +0200

    New Feature: Implement 'inverse' routing
    You can now conveniently route *from* a chosen bus or track *to* the selected one(s).
    
    So, for example, if you need "Audio Track 1" to route to x3 buses, simply select the *x3 buses* instead, run the script, use 'Route from a new/existing track/bus...', and then select the track/bus you want to route from.

--- Aux_Send_Router.lua
+++ Aux_Send_Router.lua
@@ -43,7 +43,7 @@ function factory () return function ()
     return
   end
 
-  if routes:size() == 1 then -- Check if only one route (track or bus) is currently selected; used later for final popup...
+  if routes:size() == 1 then -- Used later for final popup...
     single_route_selected = true
   end
 
@@ -54,68 +54,152 @@ function factory () return function ()
     selected_names[r:name()] = true
   end
 
-  -------------- Step 3. Gather Buses for Dropdown Menu --------------
-
-  local bus_values = {}
-  local bus_map = {}
-
-  bus_values["--- Create New Bus ---"] = "new" -- Due to the inclusion of "---" at the beginning, this will be shown as the topmost/'default' setting (~99% of the time).
-
-  for r in Session:get_routes():iter() do
-    local t = r:to_track()
-
-    if t:isnil() -- Indicates that this particular route is a *bus* (not a track).
-       and r ~= Session:master_out() -- Again, don't include the Master bus.
-       and r:n_inputs():n_audio() > 0
-       and not selected_names[r:name()] -- Prevents self-routing/feedback scenarios. (-Which Ardour does NOT block if you make an Aux Send via a script like this! :O)
-    then                                                                                                                  -- Will probably inform Robin (x42) about this...
-      local name = r:name()
-      bus_values[name] = name -- Add appropriate bus-name to table used for dd-menu.
-      bus_map[name] = r
-    end
-  end
-
-  -------------- Step 4. Initiate Main Popup Window --------------
+  -------------- Step 3: Create Initial Popup --------------
 
   local dialog = LuaDialog.Dialog(
-    "Aux Send Router (v1.0)",
+    "Aux Send Router (v1.0) - Popup 1 of 2",
     {
-      { type = "heading", title = "Please select the bus you want to route to:" },
-      {
-        type = "dropdown",
-        key = "bus",
-        title = "Destination Bus",
-        values = bus_values
-      },
-      {
-        type = "fader",
-        key = "gain",
-        title = "Aux Send Level",
-        default = 0 -- dB
+      { type = "heading", title = "What routing would you like to achieve?" },
+      { type = "dropdown",
+        key = "routing_direction",
+        title = "Route Direction:",
+        values = {
+          ["Option 1 - Route to a new/existing bus..."] = "route_to_bus",
+          ["Option 2 - Route from a new/existing track/bus..."] = "route_from_track_or_bus"
+        },
       }
     }
   )
 
-  local dialog_result = dialog:run()
-  if not dialog_result then return end -- Safeguard; not really necessary.
+  local popup1_result = dialog:run()
+  if not popup1_result then return end -- Safeguard; not really necessary(?).
 
-  -------------- Step 5. Save the session --------------
+  -------------- Step 4: Check if Ardour won't be able to make the Connection(s) --------------
+
+  if popup1_result["routing_direction"] == "route_from_track_or_bus" then
+
+    for r in routes:iter() do
+      local t = r:to_track()
+
+      if not t:isnil() then -- Indicates that this particular route is a *track*; not allowed in this case!
+
+        LuaDialog.Message(
+          "Track(s) Selected!",
+          "Ardour cannot route an Aux Send to a track!\n\nPlease select ONLY buses for this action.",
+          LuaDialog.MessageType.Warning,
+          LuaDialog.ButtonType.Close
+        ):run()
+        return
+
+      end
+    end
+  end
+
+  -------------- Step 5. Gather Necessary Routes for Dropdown Menu, as Required --------------
+
+  local route_values = {}
+  local route_map = {}
+
+  for r in Session:get_routes():iter() do
+    local t = r:to_track()
+
+    if t:isnil() -- Indicates that this particular route is a *bus* (not a track); collect buses for *either* selected option...
+      and r ~= Session:master_out() -- Again, don't include the Master bus.
+      and r:n_inputs():n_audio() > 0
+      and not selected_names[r:name()] -- Prevents self-routing/feedback scenarios. (-Which Ardour does NOT block if you make an Aux Send via a script like this! :O)
+    then                                                                                                               -- Will probably inform Robin (x42) about this...
+      local name = r:name()
+      route_values[name] = name -- Add appropriate bus-name to table used for dd-menu.
+      route_map[name] = r
+    end
+
+    if popup1_result["routing_direction"] == "route_from_track_or_bus" then -- Add tracks as well, but ONLY for this option...
+      if not t:isnil() -- Indicates that this particular route is a *track*.
+      and r:n_inputs():n_audio() > 0
+      and not selected_names[r:name()] -- Prevents self-routing/feedback scenarios.
+      then
+        local name = r:name()
+        route_values[name] = name -- Add appropriate bus-name to table used for dd-menu.
+        route_map[name] = r
+      end
+    end
+  end
+
+  -- Add options for creating a new track and/or bus:
+  route_values["--- Create New Bus ---"] = "new_bus" -- Due to the inclusion of "---" at the beginning, this will be shown as the topmost/'default' setting (~99% of the time).
+
+  if popup1_result["routing_direction"] == "route_from_track_or_bus" then
+    route_values["--- Create New Track ---"] = "new_track"
+  end
+
+  -------------- Step 6. Create Secondary Popup, as Required --------------
+
+  local popup2_result
+
+  if popup1_result["routing_direction"] == "route_to_bus" then
+
+    local dialog = LuaDialog.Dialog(
+      "Aux Send Router (v1.0)",
+      {
+        { type = "heading", title = "Please select the bus you want to route to:" },
+        { type = "dropdown",
+          key = "route_choice",
+          title = "Destination Route",
+          values = route_values
+        },
+        { type = "fader",
+          key = "gain",
+          title = "Aux Send Level",
+          default = 0 -- 0 dB
+        }
+      }
+    )
+
+    popup2_result = dialog:run()
+
+  elseif popup1_result["routing_direction"] == "route_from_track_or_bus" then -- Being verbose here... : P
+
+    local dialog = LuaDialog.Dialog(
+      "Aux Send Router (v1.0)",
+      {
+        { type = "heading", title = "Please select the track or bus you want to route from:" },
+        { type = "dropdown",
+          key = "route_choice",
+          title = "Destination Route",
+          values = route_values
+        },
+        { type = "fader",
+          key = "gain",
+          title = "Aux Send Level",
+          default = 0 -- 0 dB
+        }
+      }
+    )
+
+    popup2_result = dialog:run()
+
+  end
+
+  if not popup2_result then return end
+
+  -------------- Step 7. Save the session --------------
 
   Session:save_state("", false, false, false, false, false) -- Precautionary; Just in case something goes wrong and Ardour crashes somehow.
 
-  -------------- Step 6. Establish the Selected Bus --------------
+  -------------- Step 8. Establish the Selected Track or Bus --------------
 
-  local destination_bus
+  local destination_route
 
-  if dialog_result["bus"] == "new" then -- Create (and ultimately route) to a new bus if that's what the user wants...
-    local chn = 2                                                            -- Following some of Robin's logic here...
+  if popup2_result["route_choice"] == "new_bus" then -- Create a new bus, if required; following some of Robin's logic here...
+
+    local chn = 2
     local mst = Session:master_out()
     if not mst:isnil() then
       chn = mst:n_inputs():n_audio()
     end
     if chn == 0 then chn = 2 end
 
-    local new_bus = Session:new_audio_route(
+    local create_bus = Session:new_audio_route(
       chn, chn,
       ARDOUR.RouteGroup(),
       1, "",
@@ -123,12 +207,32 @@ function factory () return function ()
       ARDOUR.PresentationInfo.max_order
     )
 
-    destination_bus = new_bus:front()
+    destination_route = create_bus:front()
+
+  elseif popup2_result["route_choice"] == "new_track" then -- Create a new track, if required...
+
+    local chn = 2
+    local mst = Session:master_out()
+    if not mst:isnil() then
+      chn = mst:n_inputs():n_audio()
+    end
+    if chn == 0 then chn = 2 end
+
+    local create_track = Session:new_audio_track(
+      chn, chn,
+      ARDOUR.RouteGroup(),
+      1, "",
+      ARDOUR.PresentationInfo.max_order,
+      ARDOUR.TrackMode.Normal
+    )
+
+    destination_route = create_track:front()
+
   else
-    destination_bus = bus_map[dialog_result["bus"]] -- Establish the specific, already-existing bus to use.
+    destination_route = route_map[popup2_result["route_choice"]] -- Establish the specific, already-existing, chosen track or bus to use.
   end
 
-  if not destination_bus then -- Something went wrong; not really necessary(?).
+  if not destination_route then -- Something went wrong; not really necessary(?).
     LuaDialog.Message(
       "Error!",
       "The chosen bus was somehow not resolved... O___o",
@@ -138,99 +242,208 @@ function factory () return function ()
     return
   end
 
-  -------------- Step 7. Convert Fader dB to 0-to-1 Value --------------
+  -------------- Step 9. Convert Fader dB to 0-to-1 Value --------------
 
-  local gain_db = dialog_result["gain"]
+  local gain_db = popup2_result["gain"]
   local linear_gain = 10 ^ (gain_db / 20)
 
-  -------------- Step 8. Add Aux Sends --------------
+  -------------- Step 10. Add Aux Sends --------------
 
   local created_count = 0 -- Counter to count created (i.e. new) Aux Sends.
   local existing_count = 0 -- Counter to count existing Aux Sends.
 
-  for r in routes:iter() do
+  if popup1_result["routing_direction"] == "route_to_bus" then
 
-    local before = 0 -- Count sends BEFORE attempting to add a new Aux Send...
-    while true do
-      local s = r:nth_send(before)
-      if s:isnil() then break end
-      before = before + 1
-    end
+    for r in routes:iter() do
 
-    r:add_aux_send(destination_bus, r:main_outs()) -- Attempt* to add Aux Send (*one to the chosen bus might already exist)
-
-    local after = 0 -- Count sends AFTER attempting to add a new Aux Send...
-    while true do
-      local s = r:nth_send(after)
-      if s:isnil() then break end
-      after = after + 1
-    end
-
-    if after > before then -- Did Ardour actually create a new send? ...
-      created_count = created_count + 1 -- If so, then add to the right counter.
-
-      local lvl = r:send_level_controllable(before, false)
-      if not lvl:isnil() then
-        lvl:set_value(linear_gain, 1.0) -- Set the gain of the Aux Send, using the appropriately-converted "linear_gain" value.
+      local before = 0 -- Count sends BEFORE attempting to add a new Aux Send...
+      while true do
+        local s = r:nth_send(before)
+        if s:isnil() then break end
+        before = before + 1
       end
-    else
-      existing_count = existing_count + 1 -- If not, then add to the other counter.
+
+      r:add_aux_send(destination_route, r:main_outs()) -- Attempt* to add Aux Send (*one to the chosen bus might already exist)
+
+      local after = 0 -- Count sends AFTER attempting to add a new Aux Send...
+      while true do
+        local s = r:nth_send(after)
+        if s:isnil() then break end
+        after = after + 1
+      end
+
+      if after > before then -- Did Ardour actually create a new send? ...
+        created_count = created_count + 1 -- If so, then add to the right counter.
+
+        local lvl = r:send_level_controllable(before, false)
+        if not lvl:isnil() then
+          lvl:set_value(linear_gain, 1.0) -- Set the gain of the Aux Send, using the appropriately-converted "linear_gain" value.
+        end
+      else
+        existing_count = existing_count + 1 -- If not, then add to the other counter.
+      end
+    end
+
+  elseif popup1_result["routing_direction"] == "route_from_track_or_bus" then
+
+    -- First do some name sorting, for convenience:
+    local route_array = {}
+
+    for r in routes:iter() do
+      table.insert(route_array, r)
+    end
+
+    table.sort(route_array, function(a, b)
+      return a:name():lower() < b:name():lower()
+    end)
+
+    local source = destination_route  -- Name-swap; the chosen track or bus we route FROM.
+  
+    for _, target in ipairs(route_array) do -- Again, should only be a *bus or buses* at this point...
+
+      local before = 0
+      while true do
+        local s = source:nth_send(before)
+        if s:isnil() then break end
+        before = before + 1
+      end
+  
+      -- Attempt to add aux send FROM source TO this selected bus
+      source:add_aux_send(target, source:main_outs())
+  
+      -- Count sends AFTER
+      local after = 0
+      while true do
+        local s = source:nth_send(after)
+        if s:isnil() then break end
+        after = after + 1
+      end
+  
+      if after > before then
+        created_count = created_count + 1
+  
+        local lvl = source:send_level_controllable(before, false)
+        if not lvl:isnil() then
+          lvl:set_value(linear_gain, 1.0)
+        end
+      else
+        existing_count = existing_count + 1
+      end
     end
   end
 
-  -------------- Step 9. Present Final Popup --------------
+  -------------- Step 11. Present Final, Conditional Popup --------------
 
-  if single_route_selected then -- Check if single track/bus is/was selected...
+  if single_route_selected then -- Check if single track/bus is/was selected.
 
-    if created_count == 0 then -- Check if no Aux Sends were added...
-      LuaDialog.Message(
-        "No Aux Send is needed!",
-        string.format(
-          "The selected route already has an Aux Send to %s!\n",
-          destination_bus:name()
-        ),
-        LuaDialog.MessageType.Info,
-        LuaDialog.ButtonType.Close
-      ):run()
-    else
-      LuaDialog.Message(
-        "Success!",
-        string.format(
-          "The selected track or bus was successfully routed to %s!\n",
-          destination_bus:name()
-        ),
-        LuaDialog.MessageType.Info,
-        LuaDialog.ButtonType.Close
-      ):run()
-    end
-      
-  else -- If more than one route is/was selected...
+    if created_count == 0 then -- Check if no Aux Sends were created.
 
-    if created_count == 0 then -- Did all already have an Aux Send to the chosen bus? ...
+      if popup1_result["routing_direction"] == "route_from_track_or_bus" then -- Check if "Option 2" had been selected in Popup 1.
         LuaDialog.Message(
-        "No Aux Sends are needed!",
-        string.format(
-          "All of the selected tracks/buses already have an Aux Send to %s!\n",
-          destination_bus:name()
-        ),
-        LuaDialog.MessageType.Info,
-        LuaDialog.ButtonType.Close
-      ):run()
+          "No Aux Send is needed!",
+          string.format(
+            "The selected bus is already receiving an Aux Send from %s!\n",
+            destination_route:name()
+          ),
+          LuaDialog.MessageType.Info,
+          LuaDialog.ButtonType.Close
+        ):run()
+      else
+        LuaDialog.Message(
+          "No Aux Send is needed!",
+          string.format(
+            "The selected route already has an Aux Send to %s!\n",
+            destination_route:name()
+          ),
+          LuaDialog.MessageType.Info,
+          LuaDialog.ButtonType.Close
+        ):run()
+      end
+
     else
-      LuaDialog.Message(
-        "Success!",
-        string.format(
-          "All of the selected tracks/buses now have Aux Sends to %s!\n\n\nNew Aux Send(s) created: %d\n\nAux Send(s) that already existed: %d\n",
-          destination_bus:name(),
-          created_count,
-          existing_count
-        ),
-        LuaDialog.MessageType.Info,
-        LuaDialog.ButtonType.Close
-      ):run()
+
+      if popup1_result["routing_direction"] == "route_from_track_or_bus" then
+        LuaDialog.Message(
+          "Success!",
+          string.format(
+            "The selected bus is now receiving an Aux Send from %s!\n",
+            destination_route:name()
+          ),
+          LuaDialog.MessageType.Info,
+          LuaDialog.ButtonType.Close
+        ):run()
+      else
+        LuaDialog.Message(
+          "Success!",
+          string.format(
+            "The selected route now has an Aux Send to %s!\n",
+            destination_route:name()
+          ),
+          LuaDialog.MessageType.Info,
+          LuaDialog.ButtonType.Close
+        ):run()
+      end
+
+    end
+
+  else -- If more than one route is/was selected.
+
+    if created_count == 0 then
+
+      if popup1_result["routing_direction"] == "route_from_track_or_bus" then
+        LuaDialog.Message(
+          "No Aux Sends are needed!",
+          string.format(
+            "All of the selected buses are already receiving an Aux Send from %s!\n",
+            destination_route:name()
+          ),
+          LuaDialog.MessageType.Info,
+          LuaDialog.ButtonType.Close
+        ):run()
+      else
+        LuaDialog.Message(
+          "No Aux Sends are needed!",
+          string.format(
+            "All of the selected routes already have an Aux Send to %s!\n",
+            destination_route:name()
+          ),
+          LuaDialog.MessageType.Info,
+          LuaDialog.ButtonType.Close
+        ):run()
+      end
+
+    else -- At least one new Aux Send created.
+
+      if popup1_result["routing_direction"] == "route_from_track_or_bus" then
+        LuaDialog.Message(
+          "Success!",
+          string.format(
+            "All of the selected buses are now receiving an Aux Send from %s!\n\n\nNew Aux Send(s) created: %d\n\nAux Send(s) that already existed: %d\n",
+            destination_route:name(),
+            created_count,
+            existing_count
+          ),
+          LuaDialog.MessageType.Info,
+          LuaDialog.ButtonType.Close
+        ):run()
+      else
+        LuaDialog.Message(
+          "Success!",
+          string.format(
+            "All of the selected tracks/buses now have an Aux Send to %s!\n\n\nNew Aux Send(s) created: %d\n\nAux Send(s) that already existed: %d\n",
+            destination_route:name(),
+            created_count,
+            existing_count
+          ),
+          LuaDialog.MessageType.Info,
+          LuaDialog.ButtonType.Close
+        ):run()
+      end
+
     end
 
   end
+
 end end
 
 -- BONUS; Manifest a button icon ("AUX"); also based on an example from Robin:
